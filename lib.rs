cec6bc14f4531b94fb1672d3a45b7069a2b81cac
Jeremy Boetticher <me4jeremyboett@gmail.com>
2023-10-27 19:08:50 -0400

    Added prediction market calls

--- lib.rs
+++ lib.rs
@@ -1,7 +1,27 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
+use ink::primitives::AccountId;
+use sp_runtime::MultiAddress;
+
+/*
+
+Zeit Fund should:
+- Give a single account the ability to use a smart contract to buy/redeem shares in ZTG
+- Force the single account to have at least X amount of ZTG to do anything
+- Allow users to buy shares of the smart contract
+- Allow users to redeem shares of the smart contract
+
+
+NOTE:
+No dynamic insert of funds. There is a period where funds are added and afterwards no more.
+Users cannot force liquidation.
+Users that wish to exit can only resell the ERC20 token, not liquidate for the individual market positions.
+
+*/
+
 #[ink::contract]
 mod zeit_fund {
+    use ink::storage::Mapping;
 
     /// Defines the storage of your contract.
     /// Add new fields to the below struct in order
@@ -9,36 +29,233 @@ mod zeit_fund {
     #[ink(storage)]
     pub struct ZeitFund {
         /// Stores a single `bool` value on the storage.
-        value: bool,
+        manager: AccountId,
+        /// Total token supply.
+        total_supply: Balance,
+        /// Mapping from owner to number of owned token.
+        balances: Mapping<AccountId, Balance>,
+        /// Mapping of the token amount which an account is allowed to withdraw
+        /// from another account.
+        allowances: Mapping<(AccountId, AccountId), Balance>,
     }
 
+    // region: Events & Errors
+
+    /// Event emitted when a token transfer occurs.
+    #[ink(event)]
+    pub struct Transfer {
+        #[ink(topic)]
+        from: Option<AccountId>,
+        #[ink(topic)]
+        to: Option<AccountId>,
+        value: Balance,
+    }
+
+    /// Event emitted when an approval occurs that `spender` is allowed to withdraw
+    /// up to the amount of `value` tokens from `owner`.
+    #[ink(event)]
+    pub struct Approval {
+        #[ink(topic)]
+        owner: AccountId,
+        #[ink(topic)]
+        spender: AccountId,
+        value: Balance,
+    }
+
+    /// The ERC-20 error types.
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        /// Returned if not enough balance to fulfill a request is available.
+        InsufficientBalance,
+        /// Returned if not enough allowance to fulfill a request is available.
+        InsufficientAllowance,
+        /// Returned if only the manager is allowed to call the function.
+        OnlyManagerAllowed,
+    }
+
+    /// The ERC-20 result type.
+    pub type Result<T> = core::result::Result<T, Error>;
+
+    // endregion
+
+    // TODO: separate impl of ERC20 trait
+
     impl ZeitFund {
+        // TODO: turn into ERC-20 also
+
         /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new(manager: AccountId, total_shares: Balance) -> Self {
+
+            // Give the zero address itself the total supply, to be distributed later
+            let mut balances = Mapping::default();
+            balances.insert(AccountId::from([0; 32]), &total_shares);
+
+            Self {
+                manager,
+                total_supply: total_shares,
+                balances,
+                allowances: Default::default(),
+            }
+
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
+        // region: ERC-20
+
+        /// Returns the total token supply.
+        #[ink(message)]
+        pub fn total_supply(&self) -> Balance {
+            self.total_supply
+        }
+
+        /// Returns the account balance for the specified `owner`.
         ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
+        /// Returns `0` if the account is non-existent.
+        #[ink(message)]
+        pub fn balance_of(&self, owner: AccountId) -> Balance {
+            self.balance_of_impl(&owner)
         }
 
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
-        #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        /// Returns the account balance for the specified `owner`.
+        ///
+        /// Returns `0` if the account is non-existent.
+        ///
+        /// # Note
+        ///
+        /// Prefer to call this method over `balance_of` since this
+        /// works using references which are more efficient in Wasm.
+        #[inline]
+        fn balance_of_impl(&self, owner: &AccountId) -> Balance {
+            self.balances.get(owner).unwrap_or_default()
         }
 
-        /// Simply returns the current value of our `bool`.
+        /// Returns the amount which `spender` is still allowed to withdraw from `owner`.
+        ///
+        /// Returns `0` if no allowance has been set.
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
+        pub fn allowance(&self, owner: AccountId, spender: AccountId) -> Balance {
+            self.allowance_impl(&owner, &spender)
+        }
+
+        /// Returns the amount which `spender` is still allowed to withdraw from `owner`.
+        ///
+        /// Returns `0` if no allowance has been set.
+        ///
+        /// # Note
+        ///
+        /// Prefer to call this method over `allowance` since this
+        /// works using references which are more efficient in Wasm.
+        #[inline]
+        fn allowance_impl(&self, owner: &AccountId, spender: &AccountId) -> Balance {
+            self.allowances.get((owner, spender)).unwrap_or_default()
+        }
+
+        /// Transfers `value` amount of tokens from the caller's account to account `to`.
+        ///
+        /// On success a `Transfer` event is emitted.
+        ///
+        /// # Errors
+        ///
+        /// Returns `InsufficientBalance` error if there are not enough tokens on
+        /// the caller's account balance.
+        #[ink(message)]
+        pub fn transfer(&mut self, to: AccountId, value: Balance) -> Result<()> {
+            let from = self.env().caller();
+            self.transfer_from_to(&from, &to, value)
+        }
+
+        /// Allows `spender` to withdraw from the caller's account multiple times, up to
+        /// the `value` amount.
+        ///
+        /// If this function is called again it overwrites the current allowance with
+        /// `value`.
+        ///
+        /// An `Approval` event is emitted.
+        #[ink(message)]
+        pub fn approve(&mut self, spender: AccountId, value: Balance) -> Result<()> {
+            let owner = self.env().caller();
+            self.allowances.insert((&owner, &spender), &value);
+            self.env().emit_event(Approval {
+                owner,
+                spender,
+                value,
+            });
+            Ok(())
+        }
+
+        /// Transfers `value` tokens on the behalf of `from` to the account `to`.
+        ///
+        /// This can be used to allow a contract to transfer tokens on ones behalf and/or
+        /// to charge fees in sub-currencies, for example.
+        ///
+        /// On success a `Transfer` event is emitted.
+        ///
+        /// # Errors
+        ///
+        /// Returns `InsufficientAllowance` error if there are not enough tokens allowed
+        /// for the caller to withdraw from `from`.
+        ///
+        /// Returns `InsufficientBalance` error if there are not enough tokens on
+        /// the account balance of `from`.
+        #[ink(message)]
+        pub fn transfer_from(
+            &mut self,
+            from: AccountId,
+            to: AccountId,
+            value: Balance,
+        ) -> Result<()> {
+            let caller = self.env().caller();
+            let allowance = self.allowance_impl(&from, &caller);
+            if allowance < value {
+                return Err(Error::InsufficientAllowance);
+            }
+            self.transfer_from_to(&from, &to, value)?;
+            self.allowances
+                .insert((&from, &caller), &(allowance - value));
+            Ok(())
+        }
+
+        /// Transfers `value` amount of tokens from the caller's account to account `to`.
+        ///
+        /// On success a `Transfer` event is emitted.
+        ///
+        /// # Errors
+        ///
+        /// Returns `InsufficientBalance` error if there are not enough tokens on
+        /// the caller's account balance.
+        fn transfer_from_to(
+            &mut self,
+            from: &AccountId,
+            to: &AccountId,
+            value: Balance,
+        ) -> Result<()> {
+            let from_balance = self.balance_of_impl(from);
+            if from_balance < value {
+                return Err(Error::InsufficientBalance);
+            }
+
+            self.balances.insert(from, &(from_balance - value));
+            let to_balance = self.balance_of_impl(to);
+            self.balances.insert(to, &(to_balance + value));
+            self.env().emit_event(Transfer {
+                from: Some(*from),
+                to: Some(*to),
+                value,
+            });
+            Ok(())
+        }
+
+        // endregion
+
+
+
+        #[inline]
+        fn only_manager(&self) -> Result<()> {
+            if self.env().caller() != self.manager {
+                return Err(Error::OnlyManagerAllowed)
+            }
+            Ok(())
         }
     }
 
@@ -47,96 +264,69 @@ mod zeit_fund {
     /// The below code is technically just normal Rust code.
     #[cfg(test)]
     mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
+        // Imports all the definitions from the outer scope so we can use them here.
+        // use super::*;
 
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let zeit_fund = ZeitFund::default();
-            assert_eq!(zeit_fund.get(), false);
-        }
-
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut zeit_fund = ZeitFund::new(false);
-            assert_eq!(zeit_fund.get(), false);
-            zeit_fund.flip();
-            assert_eq!(zeit_fund.get(), true);
-        }
+        // TODO: write tests if you have time
     }
+}
 
-
-    /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
+#[derive(scale::Encode, scale::Decode)]
+enum RuntimeCall {
+    /// This index can be found by investigating runtime configuration. You can check the
+    /// pallet order inside `construct_runtime!` block and read the position of your
+    /// pallet (0-based).
     ///
-    /// When running these you need to make sure that you:
-    /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
-    /// - Are running a Substrate node which contains `pallet-contracts` in the background
-    #[cfg(all(test, feature = "e2e-tests"))]
-    mod e2e_tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
+    /// https://github.com/zeitgeistpm/zeitgeist/blob/3d9bbff91219bb324f047427224ee318061a6d43/runtime/common/src/lib.rs#L254-L363
+    ///
+    /// [See here for more.](https://substrate.stackexchange.com/questions/778/how-to-get-pallet-index-u8-of-a-pallet-in-runtime)
+    #[codec(index = 40)]
+    AssetManager(AssetManagerCall),
+    #[codec(index = 57)]
+    PredictionMarkets(PredictionMarketsCall),
+}
 
-        /// A helper function used for calling contract messages.
-        use ink_e2e::build_message;
+#[derive(scale::Encode, scale::Decode)]
+enum AssetManagerCall {
+    // https://github.com/open-web3-stack/open-runtime-module-library/blob/22a4f7b7d1066c1a138222f4546d527d32aa4047/currencies/src/lib.rs#L129-L131C19
+    #[codec(index = 0)]
+    Transfer {
+        dest: MultiAddress<AccountId, ()>,
+        currency_id: ZeitgeistAsset,
+        #[codec(compact)]
+        amount: u128,
+    },
+}
 
-        /// The End-to-End test `Result` type.
-        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-        /// We test that we can upload and instantiate the contract using its default constructor.
-        #[ink_e2e::test]
-        async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = ZeitFundRef::default();
-
-            // When
-            let contract_account_id = client
-                .instantiate("zeit_fund", &ink_e2e::alice(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            // Then
-            let get = build_message::<ZeitFundRef>(contract_account_id.clone())
-                .call(|zeit_fund| zeit_fund.get());
-            let get_result = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            Ok(())
-        }
-
-        /// We test that we can read and write a value from the on-chain contract contract.
-        #[ink_e2e::test]
-        async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = ZeitFundRef::new(false);
-            let contract_account_id = client
-                .instantiate("zeit_fund", &ink_e2e::bob(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            let get = build_message::<ZeitFundRef>(contract_account_id.clone())
-                .call(|zeit_fund| zeit_fund.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            // When
-            let flip = build_message::<ZeitFundRef>(contract_account_id.clone())
-                .call(|zeit_fund| zeit_fund.flip());
-            let _flip_result = client
-                .call(&ink_e2e::bob(), flip, 0, None)
-                .await
-                .expect("flip failed");
-
-            // Then
-            let get = build_message::<ZeitFundRef>(contract_account_id.clone())
-                .call(|zeit_fund| zeit_fund.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), true));
-
-            Ok(())
-        }
+#[derive(scale::Encode, scale::Decode)]
+enum PredictionMarketsCall {
+    #[codec(index = 5)]
+    BuyCompleteSet {
+        #[codec(compact)]
+        market_id: u128,
+        #[codec(compact)]
+        amount: u128,
+    },
+    #[codec(index = 12)]
+    RedeemShares {
+        #[codec(compact)]
+        market_id: u128
+    },
+    #[codec(index = 15)]
+    SellCompleteSet {
+        #[codec(compact)]
+        market_id: u128,
+        #[codec(compact)]
+        amount: u128,
     }
+}
+
+#[derive(scale::Encode, scale::Decode, Clone, PartialEq)]
+enum ZeitgeistAsset {
+    CategoricalOutcome, //(MI, CategoryIndex),
+    ScalarOutcome,      //(MI, ScalarPosition),
+    CombinatorialOutcome,
+    PoolShare, //(SerdeWrapper<PoolId>),
+    Ztg,       // default
+    ForeignAsset(u32),
 }
